0ea7a8e614cec05ac00882e3842dfbb301fe2cbf
Prelang Builder <builder@prelang.com>
2014-07-04 07:54:49 +0000

    Added #vote action to set model before_action in the votable model's controller

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,7 +3,7 @@ class ActivitiesController < ApplicationController
   #->Prelang (scaffolding:rails/scope_to_user)
   before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
 
-  before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  before_action :set_activity, only: [:show, :edit, :update, :destroy, :vote]
 
   # GET /activities
   # GET /activities.json
